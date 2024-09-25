55658704436420c7f8e418f4a77fb5d2065f9bcc
mqmotiwala <mqmotiwala@gmail.com>
2024-09-24 21:57:23 -0700

    stop tracking log file

--- main.py
+++ main.py
@@ -6,19 +6,16 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     try:
-        if request.is_json:
-            data = request.get_json()
-            log_booking(data)
-            return jsonify({"message": "Webhook received!"}), 200
-        else:
-            logger.error(f"Unsupported content type: {request.content_type}")
-            logger.error(f"Request body: {request.data}")
-            return jsonify({"error": "Unsupported Media Type. Please send JSON data."}), 415
+        data = request.get_json()
+        handle_booking(data)
+
+        return jsonify({"message": "Webhook received!"}), 200
+
     except Exception as e:
         logger.error(f"Error processing request: {e}")
         return jsonify({"error": "Bad Request", "details": str(e)}), 400
 
-def log_booking(data):
+def handle_booking(data):
     logger.info(f"New booking: {data}")
 
 if __name__ == "__main__":
